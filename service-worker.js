1e6752b9e1ad3e7eebb8f2fa1b4e5304af19977a
Sophia <sophiakirstentorrenueva@gmail.com>
2025-04-23 12:07:31 +0800

    trying to set up a PWA

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', function (event) {
+    console.log('[Service Worker] Installed');
+});
+  
+  self.addEventListener('fetch', function (event) {
+    // Can be blank or basic
+});
+  
---
+++ src/index.js
@@ -0,0 +1,10 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('./service-worker.js') // This must start with '/'
+      .then(function (registration) {
+        console.log('✅ Service Worker registered with scope:', registration.scope);
+      }, function (err) {
+        console.log('❌ Service Worker registration failed:', err);
+      });
+  });
+}
