87e5a1d42db57d87575b0376389ee01cfd95f0a2
Sophia <sophiakirstentorrenueva@gmail.com>
2025-05-05 18:54:14 +0800

    toggle password

--- js/login.js
+++ js/login.js
@@ -5,6 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector(".login form");
     const assistanceBox = document.querySelector(".assistance");
     const backBtn = document.querySelector(".back-btn");
+    const passwordInput = document.getElementById('login-password');
+    const lockIcon = document.querySelector('.bxs-lock-alt');
+    const openLockIcon = document.querySelector('.bxs-lock-open-alt');
+
+    openLockIcon.style.display = 'none';
     
     if (!container) console.error("Container not found");
     if (!loginBtn) console.error("Login button not found");
@@ -12,6 +17,9 @@ document.addEventListener("DOMContentLoaded", () => {
     if (!loginForm) console.error("Login form not found");
     if (!assistanceBox) console.error("Assistance box not found");
     if (!backBtn) console.error("Back button not found");
+    if (!passwordInput) console.error("Password input not found");
+    if (!lockIcon) console.error("Closed lock icon not found");
+    if (!openLockIcon) console.error("Open lock icon not found");
     
     // Toggle between login form and assistance box
     if (assistanceBox && loginForm) {
@@ -32,6 +40,21 @@ document.addEventListener("DOMContentLoaded", () => {
         window.location.href = '../index.html';
       });
     }
+
+    if (lockIcon && openLockIcon && passwordInput) {
+      lockIcon.addEventListener('click', () => {
+        passwordInput.type = 'text';
+        lockIcon.style.display = 'none';
+        openLockIcon.style.display = 'inline';
+      });
+    
+      openLockIcon.addEventListener('click', () => {
+        passwordInput.type = 'password';
+        openLockIcon.style.display = 'none';
+        lockIcon.style.display = 'inline';
+      });
+    }
+
   });
 
   
